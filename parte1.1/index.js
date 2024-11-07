c78bf7c82799e712e683521ae1380e81be94dc40
RodrigoPinto22 <131172504+RodrigoPinto22@users.noreply.github.com>
2024-11-07 22:55:02 +0000

    Update index.js

--- parte1.1/index.js
+++ parte1.1/index.js
@@ -25,11 +25,11 @@ function placePieces(event) {
     const piece = document.createElement("div");
     piece.className = "piece";
     piece.style.backgroundColor = color;
-    piece.classList.add(`${color}piece`)
+    piece.classList.add(color)
     clickedElement.appendChild(piece);
     counter++;  
 
-    if (counter === 18) {
+    if (counter === 4) {
         const corners = document.getElementsByClassName("corner");
         const sides = document.getElementsByClassName("side");
         for (let i = 0; i < corners.length; i++) {
@@ -40,9 +40,43 @@ function placePieces(event) {
             const side = sides[i];
             side.removeEventListener("click", placePieces);
         } 
+        captureStage()
     }
 }
 
 function captureStage() {
+    console.log(counter)
+    const corners = document.getElementsByClassName("corner");
+    const sides = document.getElementsByClassName("side");
+    for (let i = 0; i < corners.length; i++) {
+        const corner = corners[i];
+        corner.addEventListener("click", playerMove);
+    }
+    for (let i = 0; i < sides.length; i++) {
+        const side = sides[i];
+        side.addEventListener("click", playerMove);
+    }
 
+    function playerMove(event) {
+        const clickedPiece = event.currentTarget.querySelector(".piece");
+        const clickedPieceColor = clickedPiece.classList[1];
+        console.log("ID of clicked cell:", clickedPieceColor);
+
+        if (clickedPieceColor === "red") {
+            if (counter % 2 != 0) {
+                
+            }
+            console.log("AS")
+            if (clickedElement && clickedElement.querySelector(".piece")) {
+                return; 
+            }
+        }
+    }
+    function movePiece() {
+
+    }
+
+    function capturePiece() {
+
+    }
 }
